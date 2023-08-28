382b3aa9b0edc46c5b9d688b54a15a5571c45ac6
Adam Bechtold <adamcbechtold@gmail.com>
2023-08-28 07:50:40 -0400

    so we have placeholder text for the introduction

--- app/components/ContentItem/Introduction/Introduction.js
+++ app/components/ContentItem/Introduction/Introduction.js
@@ -5,7 +5,7 @@ export default function Introduction() {
   return (
     <p>
       {
-        "I'm doing things that I'd like to tell you about! Eventually, this will be full of text convincing you that I'm serious about bio. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend justo non metus gravida, a commodo lacus facilisis. Fusce non arcu at leo luctus molestie. Proin tincidunt vulputate odio, vitae luctus nulla consectetur id."
+        "I'm doing things that I'd like to tell you about! For now, though, some placeholder text... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend justo non metus gravida, a commodo lacus facilisis. Fusce non arcu at leo luctus molestie. Proin tincidunt vulputate odio, vitae luctus nulla consectetur id."
       }
     </p>
   );
