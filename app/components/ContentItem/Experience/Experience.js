4db722dd83fd18215c6fb92ee63e1ed30511b83f
Adam Bechtold <adamcbechtold@gmail.com>
2023-10-25 13:52:55 -0400

    so the experience section reflects work on trailhead

--- app/components/ContentItem/Experience/Experience.js
+++ app/components/ContentItem/Experience/Experience.js
@@ -5,12 +5,18 @@ import styles from "./Experience.module.css";
 
 const experiences = [
   {
+    title: "Founder",
+    company: "Trailhead",
+    dates: "(september 2023 - present)",
+    link: "https://trailhead.adambechtold.xyz",
+  },
+  {
     title: "Product Manager",
     company: "Cosmos",
     dates: "(july 2019 - may 2023)",
   },
   {
-    title: "Product Management Co-op",
+    title: "Product & Software Co-op",
     company: "Mavrck",
     dates: "(july 2018 - december 2018)",
   },
@@ -32,34 +38,6 @@ const experiences = [
  * @return {JSX.Element} The rendered list of experiences.
  */
 export default function Experience() {
-  /**
-   * Renders an individual experience item.
-   *
-   * @param {Object} props - The props for the component.
-   * @param {string} props.title - The title of the experience.
-   * @param {string} props.company - The company of the experience.
-   * @param {string} props.dates - The dates of the experience.
-   * @return {JSX.Element} The rendered experience item.
-   */
-  function ExperienceItem({ title, company, dates }) {
-    return (
-      <div className={styles.container}>
-        <div>
-          <b> {title} </b> @ <b> {company} </b>
-        </div>
-        <div className={styles.dates}>
-          <small> {dates} </small>
-        </div>
-      </div>
-    );
-  }
-
-  ExperienceItem.propTypes = {
-    title: PropTypes.string.isRequired,
-    company: PropTypes.string.isRequired,
-    dates: PropTypes.string.isRequired,
-  };
-
   return (
     <>
       {experiences.map((experience) => (
@@ -68,8 +46,48 @@ export default function Experience() {
           title={experience.title}
           company={experience.company}
           dates={experience.dates}
+          link={experience.link}
         />
       ))}
     </>
   );
 }
+
+/**
+ * Renders an individual experience item.
+ *
+ * @param {Object} props - The props for the component.
+ * @param {string} props.title - The title of the experience.
+ * @param {string} props.company - The company of the experience.
+ * @param {string} props.dates - The dates of the experience.
+ * @param {string} props.link - The link to the company.
+ * @return {JSX.Element} The rendered experience item.
+ */
+function ExperienceItem({ title, company, dates, link }) {
+  const Company = ({ company, link }) =>
+    link ? (
+      <b>
+        <a href={link}>{company}</a>
+      </b>
+    ) : (
+      <b>{company}</b>
+    );
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <b> {title} </b> @ <Company company={company} link={link} />
+      </div>
+      <div className={styles.dates}>
+        <small> {dates} </small>
+      </div>
+    </div>
+  );
+}
+
+ExperienceItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  company: PropTypes.string.isRequired,
+  dates: PropTypes.string.isRequired,
+  link: PropTypes.string.notRequired,
+};
