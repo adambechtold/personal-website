a7330f4abff76dab4dc1765e5e270f1ae1cd77ba
Adam Bechtold <adamcbechtold@gmail.com>
2024-01-03 12:32:18 -0500

    so the site removes the biology content and better reflects my interests

--- app/components/ContentItem/Introduction/Introduction.js
+++ app/components/ContentItem/Introduction/Introduction.js
@@ -6,48 +6,20 @@ import styles from "./Introduction.module.css";
 export default function Introduction() {
   return (
     <div className={styles.container}>
-      <p>{"I'm Adam. A lover of product, engineering, design, and biology."}</p>
       <p>
         {
-          "After ~4 years in Climate Tech, I'm coming back to my biology roots to build on my experiences at Editas and Boston Scientific. I'm looking for roles that allow me to work closely with scientists, clinicians, and computational biologists to turbocharge their work."
+          "I'm Adam, a Product Manager falling back in love with technical work."
         }
       </p>
       <p>
-        {"We're almost 1/4 through the "}
-        <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC419918/">
-          Century of Biology
-        </a>
-        {
-          ", and there's still so much more to discover. Few words capture my imagination quite like these—"
-        }
+        I recently wrapped up my work at{" "}
+        <a href="https://cosmos.space">Cosmos Partners</a> and am noodling on
+        what comes next... 🤔
       </p>
-      <div className={styles.quote}>
-        <p>
-          {
-            "Imagine a flashy spaceship lands in your backyard. The door opens and you are invited to investigate everything to see what you can learn. The technology is clearly millions of years beyond what we can make."
-          }
-        </p>
-        <p>This is biology.</p>
-        <p>
-          - Ben Hubert,{" "}
-          <a href="https://berthub.eu/articles/posts/immune-system/">
-            {'"Our Amazing Immune System"'}
-          </a>
-        </p>
-      </div>
       <p>
-        {
-          "In the meantime, I'm taking courses in bioinformatics & biology, reading research papers, cooking dishes from recent trips to "
-        }
-        <a href="https://www.chilipeppermadness.com/recipes/mojo-picon/">
-          Spain
-        </a>
-        {" and "}
-        <a href="https://foreignfork.com/red-chimichurri/">Argentina</a>, and
-        working on preventing others (and myself) from{" "}
-        <a href="https://trailhead.adambechtold.xyz">
-          getting lost in the woods.
-        </a>
+        In the meantime, check out my latest projects on{" "}
+        <a href="https://github.com/adambechtold">GitHub</a> and return to this
+        site soon for more updates!
       </p>
     </div>
   );
